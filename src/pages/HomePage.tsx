0ab2131047077d7290f8bd5258aa1db1e3e1fd88
Camilla <chri.camilla@gmail.com>
2024-12-04 14:30:37 +0100

    homepage tilpasset brukerrolle

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,31 @@
-import React from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "../redux/store";
 
 const Home = () => {
+  const { user, isAuthenticated } = useSelector(
+    (state: RootState) => state.auth
+  );
+
   return (
     <div>
-      <h1>Home page</h1>
+      {!isAuthenticated && (
+        <div>
+          <h2>Create, Customize, Conquer </h2>
+          <h1>Your Dynamic CV Awaits!</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
+            eu odio nibh. Etiam laoreet ullamcorper scelerisque. Duis sagittis
+            libero lorem, fermentum consectetur velit ornare ut.
+          </p>
+        </div>
+      )}
+
+      {isAuthenticated && (
+        <div>
+          <h1>Welcome, {user?.name}!</h1>
+          <h2>Clever message here </h2>
+        </div>
+      )}
     </div>
   );
 };
