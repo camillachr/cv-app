b860716ecd7023de3b7e8c88f6b292fd07bfb013
Camilla <chri.camilla@gmail.com>
2024-12-06 12:25:43 +0100

    cv types

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,4 +1,11 @@
-export interface User extends UserPost {
+//Generisk type for alle api-responser, da dataen ligger nested i items
+export interface ApiResponse<T> {
+  cursor: string | null;
+  items: T[];
+  next_page: string | null;
+}
+
+export interface ApiGeneratedFields {
   _uuid: string;
   _created: number;
   _data_type: string;
@@ -13,12 +20,53 @@ export interface UserPost {
   username: string;
   email: string;
   password: string;
-  role?: string;
+  role: string;
 }
 
-//Generisk type for alle api-responser, da dataen ligger nested i items
-export interface ApiResponse<T> {
-  cursor: string | null;
-  items: T[];
-  next_page: string | null;
+export interface CVPost {
+  userId: string;
+  personalInfo?: {
+    name?: string;
+    email?: string;
+    phone?: string;
+  };
+  skills?: string[];
+  education?: Education[];
+  certificates?: Certificate[];
+  experience?: Experience[];
+  references?: References[];
 }
+
+export interface Education {
+  //id: string; ??
+  institution: string;
+  degree: string;
+  startYear: number;
+  endYear?: number;
+}
+
+export interface Certificate {
+  //id: string; ??
+  title: string;
+  issuer: string;
+  date: string;
+  description?: string;
+}
+export interface Experience {
+  //id: string; ??
+  company: string;
+  jobTitle: string;
+  startDate: string;
+  endDate?: string;
+  description?: string;
+}
+
+export interface References {
+  //id: string; ??
+  name: string;
+  contactInfo: string;
+  relationship: string;
+}
+
+export type User = UserPost & ApiGeneratedFields;
+export type CV = CVPost & ApiGeneratedFields;
